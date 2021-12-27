37faebd5ed87aacd3b1c01b564685f16b09a074e
poncoe <poncoe4mac@gmail.com>
2021-12-27 17:32:49 +0700

    meow commit

---
+++ src/isenkdoank/marikost/controller/Test.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package isenkdoank.marikost.controller;
+
+import isenkdoank.marikost.assets.*;
+
+/**
+ *
+ * @author poncoe
+ */
+public class Test {
+    
+}
---
+++ src/isenkdoank/marikost/view/Test.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package isenkdoank.marikost.view;
+
+import isenkdoank.marikost.assets.*;
+
+/**
+ *
+ * @author poncoe
+ */
+public class Test {
+    
+}
